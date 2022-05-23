f8c7546d3054aeaf8271493b2ec3b294f90e683d
JkaElona29 <jelona@pcso.gov.ph>
2022-05-23 15:19:23 +0800

    Add Post and Delete.

--- pcso-jk-api/Program.cs
+++ pcso-jk-api/Program.cs
@@ -15,4 +15,13 @@ app.MapGet("Hello", () => "Hello from Philippines");
 app.MapGet("Product", () => "Get Products from Philippines");
 
 
+app.MapPost("Product", (Product p) => "Product Saved in SQL");
+app.MapDelete("Product", (string name) => "Deleted");
+
+app.MapPost("Chat", (Message msg) => "Message Posted to chat SQL");
+
 app.Run();
+
+//Some Data Contracts
+public record class Message(string Name, string Msg);
+public record class Product(string Name, string Picture, int Price);
